56e01d9e4f302d7fb5a81ede97a4e305a6801624
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2018-05-29 11:19:25 +0100

    [FD] NGC-3279 Fix Graphite metrics reporting when IP address changes
    Also upgrade other dependencies to latest versions (play-ui and sbt).

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "govuk-template" % "5.18.0",
     "uk.gov.hmrc" %% "play-ui" % "7.14.0",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.5.0"
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.6.0"
   )
 
   val test: Seq[ModuleID] = testCommon("test")
