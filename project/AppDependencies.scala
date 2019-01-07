78d9259ad1ff98f86b917e7599eac3640206eeb1
Doug Clinton <doug.clinton@digital.hmrc.gov.uk>
2019-01-07 10:56:47 +0000

    [NGC-4512] Update to Play 2.5.19 and upgraded other libs to latest compatible versions.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,21 +8,21 @@ object AppDependencies {
     compile ++ test ++ integrationTest
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "govuk-template" % "5.18.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.23.0",
     "uk.gov.hmrc" %% "play-ui" % "7.22.0",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "3.7.0"
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.6.0"
   )
 
   val test: Seq[ModuleID] = testCommon("test")
 
   val integrationTest: Seq[ModuleID] = testCommon("it") ++ Seq(
-    "com.github.tomakehurst" % "wiremock" % "2.13.0" % "it"
+    "com.github.tomakehurst" % "wiremock" % "2.20.0" % "it"
   )
 
   def testCommon(scope: String) = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "3.1.0" % scope,
-    "org.scalatest" %% "scalatest" % "3.0.4" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "3.3.0" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.5" % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
