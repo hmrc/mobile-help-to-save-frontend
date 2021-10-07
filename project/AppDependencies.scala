0c8e1b7ae0589196e9eec2ac6ef26c638c20c151
Martin Rolfe <52818407+mrolfe88@users.noreply.github.com>
2021-10-07 15:50:41 +0100

    [HMA-4898] Removed deprecated dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,6 @@ import sbt.{ModuleID, _}
 object AppDependencies {
 
   private val play27Bootstrap      = "5.4.0"
-  private val govUkTemplateVersion = "5.68.0-play-27"
   private val playFrontendVersion  = "1.17.0-play-27"
 
   private val pegdownVersion       = "1.6.0"
@@ -16,7 +15,6 @@ object AppDependencies {
     compile ++ test ++ integrationTest
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "govuk-template"     % govUkTemplateVersion,
     "uk.gov.hmrc" %% "play-frontend-hmrc" % playFrontendVersion,
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % play27Bootstrap
