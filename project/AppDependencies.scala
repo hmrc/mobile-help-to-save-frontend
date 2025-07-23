5d0455ca3781e1b3b1bcaf439418ae7e44a3fc42
pdhiman3006 <176318726+pdhiman3006@users.noreply.github.com>
2025-07-23 15:09:00 +0100

    HMA-10441 scala 3 upgrade (#29)
    * HMA-10441 scala 3 upgrade
    
    * HMA-10441 scala 3 upgrade
    
    * HMA-10441 revert port no

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,12 +1,12 @@
 import play.sbt.PlayImport.ws
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val playBootstrap       = "9.11.0"
-  private val playFrontendVersion = "11.12.0"
-  private val pegdownVersion      = "1.6.0"
-  private val wireMockVersion     = "2.21.0"
+  private val playBootstrap = "9.16.0"
+  private val playFrontendVersion = "12.7.0"
+  private val flexmarkVersion = "0.64.8"
+  private val wireMockVersion = "2.21.0"
 
   lazy val appDependencies: Seq[ModuleID] =
     compile ++ test ++ integrationTest
@@ -22,8 +22,8 @@ object AppDependencies {
   val integrationTest: Seq[ModuleID] = testCommon("it") ++ Seq.empty
 
   def testCommon(scope: String) = Seq(
-    "org.pegdown" % "pegdown"                 % pegdownVersion % scope,
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % playBootstrap  % scope
+    "com.vladsch.flexmark" % "flexmark-all"           % flexmarkVersion % scope,
+    "uk.gov.hmrc"         %% "bootstrap-test-play-30" % playBootstrap   % scope
   )
 
 }
